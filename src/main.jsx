a30866d6e327e4dd6384551df37378625ef03774
Script <zgxbrielb@gmail.com>
2026-08-22 14:25:50 -0500

    Delete src/main.jsx

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import YxStore from './YxStore.jsx';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <YxStore />
-  </React.StrictMode>
-);
